9da1d2678164c45b6563ae4316ff097596449244
Matija Srnec <srnec.matija@gmail.com>
2020-11-21 12:24:01 +0100

    inheritence - extends

---
+++ inheritence/Driver.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Shape_1 = require("./Shape");
+var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
+var myShape = new Shape_1.Shape(12, 12);
+var myCircle = new Circle_1.Circle(25, 11, 13);
+var myRectangle = new Rectangle_1.Rectangle(12, 9, 20, 20);
+console.log(myShape.getInfo());
+console.log(myCircle.getInfo());
+console.log(myRectangle.getInfo());
