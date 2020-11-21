945ee3fe3bcfaa94490ee460bfb2b158a958e584
Matija Srnec <srnec.matija@gmail.com>
2020-11-21 10:15:40 +0100

    sample variables

---
+++ variables/sample-types.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 26.12;
+var firstName = "Joe";
+var lastName = "Doe";
+console.log(found);
+console.log("Grade: " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log("Hi " + firstName + " " + lastName);
---
+++ variables/sample-types.ts
@@ -0,0 +1,9 @@
+let found: boolean = true;
+let grade: number = 26.12;
+let firstName: string = "Joe";
+let lastName: string = "Doe";
+
+console.log(found);
+console.log("Grade: " + grade);
+console.log("Hi " + firstName + " " + lastName);
+console.log(`Hi ${firstName} ${lastName}`);
