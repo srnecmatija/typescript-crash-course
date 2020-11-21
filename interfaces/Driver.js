0bc33e26873150c05618853edbd2aea63502148f
Matija Srnec <srnec.matija@gmail.com>
2020-11-21 12:47:49 +0100

    interfaces

---
+++ interfaces/Driver.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CricketCoach_1 = require("./CricketCoach");
+var myCricketCoach = new CricketCoach_1.CricketCoach();
+console.log(myCricketCoach.getDailyWorkout());
---
+++ interfaces/Driver.ts
@@ -0,0 +1,4 @@
+import { CricketCoach } from "./CricketCoach";
+
+let myCricketCoach = new CricketCoach();
+console.log(myCricketCoach.getDailyWorkout());
